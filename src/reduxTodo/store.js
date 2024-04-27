49344717e25d69c8704ee75543f2f859b96c9731
Artem <atremat@gmail.com>
2024-04-27 15:13:53 +0300

    task 1 add

---
+++ src/reduxTodo/store.js
@@ -0,0 +1,35 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import todoSlice from './todoSlice';
+
+const persistConfig = {
+  key: 'todos',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoSlice);
+
+export const store = configureStore({
+  reducer: {
+    todos: persistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
---
+++ src/reduxTodo/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState: { items: [] },
+  reducers: {
+    addTodo: (state, action) => {
+      state.items = [...state.items, action.payload];
+    },
+    deleteTodo: (state, action) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
