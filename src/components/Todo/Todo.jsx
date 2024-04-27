ca50495addbbd7b592921a9071dc80bdf8b7b0f9
Mykyta Kudlai <nikiatanik@gmail.com>
2024-04-27 14:36:07 +0200

    finished

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,9 +1,31 @@
 import { Text } from 'components';
+import { GridItem } from 'components';
+import { RiEdit2Line, RiDeleteBinLine } from 'react-icons/ri';
+import style from './Todo.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from 'reduxTodo/todoSlice';
 
-export const Todo = () => {
+export const Todo = ({ index, text, id }) => {
+  const dispatch = useDispatch();
   return (
-    <Text textAlign="center" marginBottom="20">
-      TODO #
-    </Text>
+    <GridItem>
+      <div className={style.box}>
+        <Text textAlign="center" marginBottom="20">
+          TODO #{index}
+        </Text>
+
+        <Text>{text}</Text>
+        <button
+          className={style.deleteButton}
+          type="button"
+          onClick={() => dispatch(deleteTodo(id))}
+        >
+          <RiDeleteBinLine size={24} />
+        </button>
+        <button className={style.editButton} type="button">
+          <RiEdit2Line size={24} />
+        </button>
+      </div>
+    </GridItem>
   );
 };
